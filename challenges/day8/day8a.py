e81beeac9dd084e16fa3b3acea875d757bf0ebae
xingped <xingped56@gmail.com>
2016-12-07 23:31:34 -0800

    Added Day 8 Solutions

---
+++ challenges/day8/day8a.py
@@ -0,0 +1,36 @@
+display = [[0]*50 for i in range(6)]
+
+with open('input.txt') as f:
+	for line in f:
+		inst = line.split()
+		if inst[0] == 'rect':
+			xy = inst[1].split('x')
+			for y in range(0,int(xy[1])):
+				for x in range(0,int(xy[0])):
+					display[y][x] = 1
+		elif inst[0] == 'rotate':
+			if inst[1] == 'row':
+				row = int(inst[2].split('=')[1])
+				amt = int(inst[4]) % 50
+				overflow = display[row][50-amt:50]
+				for i in reversed(range(amt,50)):
+					display[row][i] = display[row][i-amt]
+				display[row][0:amt] = overflow
+			elif inst[1] == 'column':
+				col = int(inst[2].split('=')[1])
+				amt = int(inst[4]) % 6
+				overflow = []
+				for i in range(6-amt,6):
+					overflow.append(display[i][col])
+				for i in reversed(range(amt,6)):
+					display[i][col] = display[i-amt][col]
+				for i in range(0,amt):
+					display[i][col] = overflow[i]
+
+	count = 0
+	for x in range(0,50):
+		for y in range(0,6):
+			if display[y][x] == 1:
+				count += 1
+
+	print(count)
